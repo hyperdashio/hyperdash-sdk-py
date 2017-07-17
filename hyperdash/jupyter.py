999ba456e4135d032ca0340855b0aca937d740ef
Richard Artoul <richardartoul@gmail.com>
2017-07-16 22:54:17 -0700

    Fix Jupyter integration

--- hyperdash/jupyter.py
+++ hyperdash/jupyter.py
@@ -1,29 +1,27 @@
 from .sdk import monitor
 
-def ipython_required_decorator(_):
-  def decorated():
-    raise Exception("IPython is required to use monitor_cell function")
-  return decorated
 
 # Handle situation where IPython is not in the local environment OR
 # it is available, but we're not running in the context of an IPython
 # notebook.
 try:
-  from IPython.core.magic import register_cell_magic
+  from IPython.core.magic import cell_magic
+  from IPython.core.magic import magics_class
+  from IPython.core.magic import Magics
+
+  @magics_class
+  class IPythonMagicsWrapper(Magics):
+    @cell_magic
+    def monitor_cell(self, line, cell):
+      if line is None or line == "":
+        return "ERROR: Please provide a valid model name. Ex. %%monitor_cell dogs vs. cats"
+      
+      @monitor(line, use_http=True)
+      def wrapped():
+        ns = {}
+        exec(cell, self.shell.user_ns, ns)
+      wrapped()
+
 except ImportError:
-  register_cell_magic = ipython_required_decorator
-try:
-  get_ipython
-except NameError:
-  register_cell_magic = ipython_required_decorator
-
-
-@register_cell_magic
-def monitor_cell(line, cell):
-    if line is None or line == "":
-        return "ERROR: Please provide a valid model name. Ex. %%hyperdash dogs vs. cats"
-
-    @monitor(line, use_http=True)
-    def wrapped():
-        exec(cell) in globals(), locals()
-    wrapped()
+  class IPythonMagicsWrapper:
+    pass
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "0.3.3"
+version = "0.3.4"
 
 setup(
     name='hyperdash',
