b81fe9047f20ea8abf63c4cbfd448c2954d16188
Richard Artoul <richardartoul@gmail.com>
2017-08-07 20:40:58 -0700

    Fix Jupyter 2/3 issues

--- hyperdash/jupyter.py
+++ hyperdash/jupyter.py
@@ -6,27 +6,32 @@ from .sdk import monitor
 # it is available, but we're not running in the context of an IPython
 # notebook.
 try:
-  from IPython.core.magic import cell_magic
-  from IPython.core.magic import magics_class
-  from IPython.core.magic import Magics
-  from IPython.core.magic import needs_local_scope
+    from IPython.core.magic import cell_magic
+    from IPython.core.magic import magics_class
+    from IPython.core.magic import Magics
+    from IPython.core.magic import needs_local_scope
 
-  @magics_class
-  class IPythonMagicsWrapper(Magics):
-    @needs_local_scope
-    @cell_magic
-    def monitor_cell(self, line, cell, local_ns=None):
-      if line is None or line == "":
-        return "ERROR: Please provide a valid model name. Ex. %%monitor_cell dogs vs. cats"
-      
-      @monitor(line)
-      def wrapped():
-        if PY2:
-          exec cell in self.shell.user_ns, local_ns
-        else:
-          exec(cell, self.shell.user_ns, local_ns)
-      wrapped()
+    # Syntax for exec changes between Python2/3 (in Python2 its a statement
+    # and in Python3 its a function) so we have to wrap it like this to
+    # prevent pre-runtime syntax errors
+    if PY2:
+        from .jupyter_2_exec import wrapped_exec
+    else:
+        from .jupyter_3_exec import wrapped_exec
+
+    @magics_class
+    class IPythonMagicsWrapper(Magics):
+        @needs_local_scope
+        @cell_magic
+        def monitor_cell(self, line, cell, local_ns=None):
+            if line is None or line == "":
+                return "ERROR: Please provide a valid model name. Ex. %%monitor_cell dogs vs. cats"
+
+            @monitor(line)
+            def wrapped():
+                wrapped_exec(cell, self.shell.user_ns, local_ns)
+            wrapped()
 
 except ImportError:
-  class IPythonMagicsWrapper:
-    pass
+    class IPythonMagicsWrapper:
+        pass
---
+++ hyperdash/jupyter_3_exec.py
@@ -0,0 +1,2 @@
+def wrapped_exec(cell, global_ns, local_ns):
+  exec(cell, global_ns, local_ns)
